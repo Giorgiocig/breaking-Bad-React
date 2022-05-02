5f292c43e19d40310feab428e24b3c49c1e7ac30
Giorgiocig <87763885+Giorgiocig@users.noreply.github.com>
2022-05-02 12:25:10 +0200

    Add files via upload

--- src/HomeComponents/Selector.js
+++ src/HomeComponents/Selector.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+import "./Selector.css";
+
 export default function Selector() {
   const navigate = useNavigate();
   return (
-    <div className="row container container-selector flex-sb">
+    <div className="row container flex-sb">
       <div>
-        <button onClick={() => navigate("/characters")}>Characters</button>
+        <button onClick={() => navigate("/characters")} className="btnSel">
+          Characters
+        </button>
       </div>
       <div>
-        <button>Episodes</button>
+        <button onClick={() => navigate("/episodes")} className="btnSel">
+          Episodes
+        </button>
       </div>
     </div>
   );
